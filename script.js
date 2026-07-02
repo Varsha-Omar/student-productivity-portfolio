6e33e7dd3af0418d08cb5fa43061a036f6f299ea
Varsha-Omar <varshagupta2249@gmail.com>
2026-07-02 07:38:36 +0530

    Task 2 - Added JavaScript Form Validation
    Implemented JavaScript form validation for the Contact page. Added validation for required fields, email format, phone number, subject, and message. Displays a success message when the form is submitted successfully.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function showMessage() {
 alert("Welcome to Student Productivity & Portfolio Web Application");
 }
+function toggleDarkMode(){
+    document.body.classList.toggle("dark");
+}
+ 
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+
+    if (!name || !email || !phone || !subject || !message) {
+        alert("Please fill all fields!");
+        return;
+    }
+    
+    alert("Form submitted successfully!");
+    this.reset();
+});
